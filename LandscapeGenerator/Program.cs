5decf756fb35689399c5a10c099b4351ad033f9d
Павел Белов <Pavel.Belov2021@outlook.com>
2021-01-23 21:22:59 +0300

    удалил string s из Program

--- LandscapeGenerator/Program.cs
+++ LandscapeGenerator/Program.cs
@@ -18,7 +18,7 @@ namespace LandscapeGenerator
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new Form1());
-            string s =" ";
+            
 		}
 	}
 }
