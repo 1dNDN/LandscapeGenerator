a50f80208649afed56d6415b8818b48086eccead
1dNDN <idndn@inbox.ru>
2021-01-23 22:15:55 +0300

    Добавлен DirectBitmap

---
+++ LandscapeGenerator/DirectBitmap.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace LandscapeGenerator
+{
+    public class DirectBitmap : IDisposable
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public DirectBitmap(int width, int height) {
+            Width = width;
+            Height = height;
+            Bits = new int[width * height];
+            BitsHandle = GCHandle.Alloc(Bits, GCHandleType.Pinned);
+            Bitmap = new Bitmap(width, height, width * 4, PixelFormat.Format32bppPArgb,
+                BitsHandle.AddrOfPinnedObject());
+        }
+
+        public Bitmap Bitmap { get; }
+
+        public int[] Bits { get; }
+
+        public bool Disposed { get; private set; }
+
+        public int Height { get; }
+
+        public int Width { get; }
+
+        protected GCHandle BitsHandle { get; }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Dispose() {
+            if (Disposed) return;
+            Disposed = true;
+            Bitmap.Dispose();
+            BitsHandle.Free();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void SetPixel(int x, int y, Color colour) {
+            int index = x + y * Width;
+            int col = colour.ToArgb();
+
+            Bits[index] = col;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void SetPixel(int x, int y, int colour) {
+            Bits[x + y * Width] = colour;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Color GetPixel(int x, int y) {
+            int index = x + y * Width;
+            int col = Bits[index];
+            Color result = Color.FromArgb(col);
+
+            return result;
+        }
+    }
+}
--- LandscapeGenerator/Form1.cs
+++ LandscapeGenerator/Form1.cs
@@ -1,25 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace LandscapeGenerator
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-
-		private void Form1_Resize(object sender, EventArgs e) {
-            button1.Height = this.Height - 63;
-            button1.Width = this.Width - 205;
+    public partial class Form1 : Form
+    {
+        public Form1() {
+            InitializeComponent();
         }
-	}
+
+        private void Form1_Resize(object sender, EventArgs e) {
+            button1.Height = Height - 63;
+            button1.Width = Width - 205;
+        }
+    }
 }
